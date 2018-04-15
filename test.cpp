68561fa4d7d68a6d77fd108a0967979993c6f40c
Victor <victorhe8873@gmail.com>
2018-04-14 17:21:59 -0700

    Add a new comment

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@ int main()
     int input = 0;
     std::cout << "Please input \n";
     cin>>input;
-    for(int i = 1;i<= input;i++) {
+    for(int i = 1;i<= input;i++) t{
         for(int j = 0; j < input - i; j++){
             std::cout << " ";
         }
